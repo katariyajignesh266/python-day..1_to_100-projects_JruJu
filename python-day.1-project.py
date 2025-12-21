5391ab7154ab4987f5877c048b57411449755326
Jignesh Katariya <katariyajignesh266@gmail.com>
2025-12-21 18:19:57 +0530

    Initial commit

---
+++ python-day.1-project.py
@@ -0,0 +1,5 @@
+print("welcome to the band name generator.")
+city = input("which city did you grow up in ?\n")
+pet = input("what is the name of a pet?\n")
+print("your band name could: "  + city + " " + pet)
+
